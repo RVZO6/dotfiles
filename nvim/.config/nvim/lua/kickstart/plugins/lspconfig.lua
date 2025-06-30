be2120c980fcd33f7fd1d8f756a8cddcee9b1685
RVZO6 <66844656+RVZO6@users.noreply.github.com>
2025-06-30 11:34:14 -0400

    lsp

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -220,6 +220,8 @@ return {
           },
         },
         bashls = {},
+        fish_lsp = {},
+        cssls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
