97109597de84972c247a37cc5cdfcbfe37beace2
RVZO6 <66844656+RVZO6@users.noreply.github.com>
2025-07-20 14:28:40 -0400

    changes

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,9 @@
 vim.keymap.del("n", "<leader>|")
 vim.keymap.set("n", "<leader>=", "<cmd>vsplit<CR>", { desc = "Split Window Right" })
+
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true })
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true })
+vim.keymap.set("i", "<Tab>", "<C-t>", { noremap = true })
+vim.keymap.set("i", "<S-Tab>", "<C-d>", { noremap = true })
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true })
