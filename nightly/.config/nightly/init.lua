7b65ec1633ea8826a0b4c3390376a7f14939256a
RVZO6 <ryanbmb@proton.me>
2025-08-04 11:47:31 -0400

    more

--- nightly/.config/nightly/init.lua
+++ nightly/.config/nightly/init.lua
@@ -1,29 +1,5 @@
-local disabled = {
-	"gzip",
-	"zip",
-	"zipPlugin",
-	"tar",
-	"tutor",
-	"tarPlugin",
-	"getscript",
-	"getscriptPlugin",
-	"vimball",
-	"vimballPlugin",
-	"2html_plugin",
-	"matchit",
-	"matchparen",
-	"logiPat",
-	"rrhelper",
-	-- "netrw",
-	-- "netrwPlugin",
-	-- "netrwSettings",
-	-- "netrwFileHandlers",
-}
-for _, p in ipairs(disabled) do
-	vim.g["loaded_" .. p] = 1
-end
-
 -- Load core configuration
+require('disabled')
 require('options')
 require('keymaps')
 require('autocmds')
