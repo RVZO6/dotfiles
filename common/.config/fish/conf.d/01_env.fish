77f4e0ed7de1b11edf5098a309dbd5a9b4d428e6
ryan <ryanbmb@proton.me>
2025-08-07 15:57:01 -0400

    changes

--- common/.config/fish/conf.d/01_env.fish
+++ common/.config/fish/conf.d/01_env.fish
@@ -1,20 +1,13 @@
-set -Ux BUN_INSTALL "$HOME/.bun"
-set -Ux UV_PYTHON_PREFERENCE managed
-set -Ux SKIM_DEFAULT_COMMAND fd
-set -Ux XDG_CONFIG_HOME "$HOME/.config"
+set -gx BUN_INSTALL "$HOME/.bun"
+set -gx UV_PYTHON_PREFERENCE managed
+set -gx SKIM_DEFAULT_COMMAND fd
+set -g XDG_CONFIG_HOME "$HOME/.config"
 
 fish_add_path "$HOME/.bun/bin"
 fish_add_path "$HOME/.local/scripts"
 fish_add_path "$HOME/.local/share/bob/nvim-bin"
 
-switch (uname)
-    case Darwin
-        source "$XDG_CONFIG_HOME/fish/conf.d/02_macos.fish"
-    case Linux
-        source "$XDG_CONFIG_HOME/fish/conf.d/02_linux.fish"
-end
-
-set -Ux EDITOR nvim
+set -gx EDITOR nvim
 fish_add_path "$ANDROID_HOME/cmdline-tools/latest/bin"
 fish_add_path "$ANDROID_HOME/platform-tools"
 fish_add_path "$ANDROID_HOME/emulator"
